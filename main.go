651fa5641fbe71b3f69587a0d152b2ced172a37d
Philippe Milot <pmilot@turbulent.ca>
2017-06-24 15:09:58 -0400

    First working version

--- main.go
+++ main.go
@@ -10,9 +10,10 @@ const NumRows = 50
 const RowHeightPx = 20
 const NumColumns = 50
 const ColWidthPx = 20
+const GenerationTick = 16
 const (
-	ColorAlive uint32 = 0xffff0000
-	ColorDead  uint32 = 0x00000000
+	ColorAlive uint32 = 0x00000000
+	ColorDead  uint32 = 0xffffffff
 )
 
 func main() {
@@ -21,16 +22,19 @@ func main() {
 	var dragging bool
 	var dragbtn uint8
 	var event sdl.Event
-	var state [NumRows][NumColumns]bool
+	var state GameState
+	state.Started = false
 
 	sdl.Init(sdl.INIT_EVERYTHING)
 
-	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+	window, err := sdl.CreateWindow("GoGOL - Go Game Of Life", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
 		NumColumns*ColWidthPx, NumRows*RowHeightPx, sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
 	}
 	defer window.Destroy()
+
+	draw(state.Current, window)
 
 	dragging = false
 	running = true
@@ -40,37 +44,49 @@ func main() {
 			switch t := event.(type) {
 			case *sdl.QuitEvent:
 				running = false
+			case *sdl.KeyDownEvent:
+				if t.Keysym.Sym == 32 {
+					state.Started = !state.Started
+				}
 			case *sdl.MouseMotionEvent:
-				row := (t.Y / RowHeightPx)
-				col := (t.X / ColWidthPx)
-				if dragging {
-					changed = state[row][col] == (dragbtn != 1)
-					state[row][col] = dragbtn == 1
+				if !state.Started {
+					row := (t.Y / RowHeightPx)
+					col := (t.X / ColWidthPx)
+					if dragging {
+						changed = state.Current[row][col] == (dragbtn != 1)
+						state.Current[row][col] = dragbtn == 1
+					}
 				}
 			case *sdl.MouseButtonEvent:
-				if dragging {
-					if t.Button == dragbtn && t.State == 0 {
-						// dragend
-						dragging = false
-					}
-					// We don't listen to any other button while dragging
-				} else {
-					if t.State == 1 {
-						// Button is pressed, start drag
-						dragging = true
-						dragbtn = t.Button
-						row := (t.Y / RowHeightPx)
-						col := (t.X / ColWidthPx)
-						changed = state[row][col] == (dragbtn != 1)
-						state[row][col] = dragbtn == 1
+				if !state.Started {
+					if dragging {
+						if t.Button == dragbtn && t.State == 0 {
+							// dragend
+							dragging = false
+						}
+						// We don't listen to any other button while dragging
+					} else {
+						if t.State == 1 {
+							// Button is pressed, start drag
+							dragging = true
+							dragbtn = t.Button
+							row := (t.Y / RowHeightPx)
+							col := (t.X / ColWidthPx)
+							changed = state.Current[row][col] == (dragbtn != 1)
+							state.Current[row][col] = dragbtn == 1
+						}
 					}
 				}
 			}
 		}
-		if changed {
-			draw(state, window)
+		if state.Started {
+			state = step(state)
+			changed = true
 		}
-		sdl.Delay(16)
+		if changed {
+			draw(state.Current, window)
+		}
+		sdl.Delay(GenerationTick)
 	}
 
 	sdl.Quit()
